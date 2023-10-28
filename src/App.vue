996c5348ed2676a8382c58895b7675c7080cab9e
Joshua Prpich <joshuaprpich@gmail.com>
2023-10-28 14:40:50 -0500

    add ExpensesView

--- src/App.vue
+++ src/App.vue
@@ -1,207 +1,60 @@
 <template>
-  <div class="finance-wrapper">
-    <div class="finance-top">
-      <h1 class="finance-top__title">GASTOS FIJOS</h1>
-    </div>
-    <div class="finance-group" v-for="(group, i) in groups" :key="i">
-      <div class="finance-group-right">
-        <input
-          class="finance-group-right__title"
-          type="number"
-          :value="group.price"
-          @input="updatePrice($event, i)"
-        />
-      </div>
-      <div class="finance-group-left">
-        <h2 class="finance-group-left__title">{{ group.title }}</h2>
-        <h2 class="finance-group-left__price">{{ displayPrice(group) }}</h2>
-      </div>
-    </div>
-    <div class="finance-bottom">
-      <svg
-        class="finance-bottom__svg"
-        width="40"
-        height="35"
-        viewBox="0 0 40 35"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <ellipse cx="12.22" cy="11.9203" rx="12.22" ry="11.9203" fill="#23A991" />
-        <ellipse cx="28.92" cy="16.6522" rx="10.92" ry="10.6522" fill="black" />
-        <ellipse
-          cx="12.48"
-          cy="29.4203"
-          rx="5.72"
-          ry="5.57971"
-          fill="#F119F5"
-          fill-opacity="0.86"
-        />
-      </svg>
-
-      <h3 class="finance-bottom__left">VALOR TOTAL</h3>
-      <p class="finance-bottom__right">{{ expensesTotal }}</p>
-    </div>
-  </div>
+  <RouterView />
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      groups: null
-    }
-  },
-  computed: {
-    expensesTotal() {
-      let sum = 0
-      this.groups.forEach((group) => {
-        sum += group.price
-      })
-      return '$' + sum.toLocaleString('de-DE')
-    }
-  },
-  methods: {
-    updatePrice(e, idx) {
-      const value = e.target.value
-      const price = value.replace(/[.]/g, '')
-      const newPrice = +price
-      const title = `group-${idx}`
-
-      localStorage.setItem(title, newPrice)
-
-      this.groups[idx].price = newPrice
-    },
-    displayPrice(group) {
-      return '$' + group.price.toLocaleString('de-DE')
-    }
-  },
-  created() {
-    this.groups = [
-      {
-        title: 'Arriendo',
-        price: +localStorage.getItem('group-0') || 0
-      },
-      {
-        title: 'Gas',
-        price: +localStorage.getItem('group-1') || 0
-      },
-      {
-        title: 'Luz',
-        price: +localStorage.getItem('group-2') || 0
-      },
-      {
-        title: 'Agua',
-        price: +localStorage.getItem('group-3') || 0
-      },
-      {
-        title: 'Transporte',
-        price: +localStorage.getItem('group-4') || 0
-      }
-    ]
-  }
-}
+import { RouterView } from 'vue-router'
 </script>
 
-<style scoped lang="scss">
-.finance-wrapper {
-  max-width: 500px;
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
 }
-.finance-top {
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+nav {
   width: 100%;
-  height: 67px;
-  border: 3px solid rgba(178, 151, 200, 0.66);
-  margin-bottom: 30px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  &__title {
-    color: #000;
-    text-align: center;
-    font-size: 32px;
-    font-style: italic;
-    font-weight: 300;
-    line-height: normal;
-  }
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
 }
-
-.finance-group {
-  display: flex;
-  margin-bottom: 38px;
+nav a.router-link-exact-active {
+  color: var(--color-text);
 }
-
-.finance-group-left {
-  width: 140px;
-  height: 66px;
-  border-radius: 10px;
-  background-color: #530f7c;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-
-  &__title {
-    color: #fff;
-    text-align: center;
-    font-size: 16px;
-    font-style: italic;
-    font-weight: 800;
-    line-height: normal;
-  }
-
-  &__price {
-    color: #fff;
-    text-align: center;
-    font-size: 14px;
-    font-weight: 500;
-  }
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
 }
-
-.finance-group-right {
-  width: 210px;
-  height: 66px;
-  margin-right: 15px;
-  border: 1px solid #b297c8;
-  border-radius: 10px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  &__title {
-    height: 35px;
-    width: 90%;
-    color: #000;
-    text-align: center;
-    font-size: 16px;
-    font-style: italic;
-    font-weight: 300;
-    outline: none;
-    border: none;
-  }
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
 }
-
-.finance-bottom {
-  position: relative;
-  margin: 0 auto;
-  border-radius: 10px;
-  border: 1px solid #b297c8;
-  width: 299px;
-  height: 70px;
-  display: flex;
-  align-items: center;
-  &__left {
-    color: #000;
-    font-size: 12px;
-    font-weight: 400;
-    margin-left: 77px;
+nav a:first-of-type {
+  border: 0;
+}
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
   }
-  &__right {
-    margin-left: 20px;
+  .logo {
+    margin: 0 2rem 0 0;
   }
-  &__svg {
-    position: absolute;
-    top: 7px;
-    left: 7px;
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+    padding: 1rem 0;
+    margin-top: 1rem;
   }
 }
 </style>
---
+++ src/views/ExpensesView.vue
@@ -0,0 +1,207 @@
+<template>
+  <div class="finance-wrapper">
+    <div class="finance-top">
+      <h1 class="finance-top__title">GASTOS FIJOS</h1>
+    </div>
+    <div class="finance-group" v-for="(group, i) in groups" :key="i">
+      <div class="finance-group-right">
+        <input
+          class="finance-group-right__title"
+          type="number"
+          :value="group.price"
+          @input="updatePrice($event, i)"
+        />
+      </div>
+      <div class="finance-group-left">
+        <h2 class="finance-group-left__title">{{ group.title }}</h2>
+        <h2 class="finance-group-left__price">{{ displayPrice(group) }}</h2>
+      </div>
+    </div>
+    <div class="finance-bottom">
+      <svg
+        class="finance-bottom__svg"
+        width="40"
+        height="35"
+        viewBox="0 0 40 35"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <ellipse cx="12.22" cy="11.9203" rx="12.22" ry="11.9203" fill="#23A991" />
+        <ellipse cx="28.92" cy="16.6522" rx="10.92" ry="10.6522" fill="black" />
+        <ellipse
+          cx="12.48"
+          cy="29.4203"
+          rx="5.72"
+          ry="5.57971"
+          fill="#F119F5"
+          fill-opacity="0.86"
+        />
+      </svg>
+
+      <h3 class="finance-bottom__left">VALOR TOTAL</h3>
+      <p class="finance-bottom__right">{{ expensesTotal }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      groups: null
+    }
+  },
+  computed: {
+    expensesTotal() {
+      let sum = 0
+      this.groups.forEach((group) => {
+        sum += group.price
+      })
+      return '$' + sum.toLocaleString('de-DE')
+    }
+  },
+  methods: {
+    updatePrice(e, idx) {
+      const value = e.target.value
+      const price = value.replace(/[.]/g, '')
+      const newPrice = +price
+      const title = `group-${idx}`
+
+      localStorage.setItem(title, newPrice)
+
+      this.groups[idx].price = newPrice
+    },
+    displayPrice(group) {
+      return '$' + group.price.toLocaleString('de-DE')
+    }
+  },
+  created() {
+    this.groups = [
+      {
+        title: 'Arriendo',
+        price: +localStorage.getItem('group-0') || 0
+      },
+      {
+        title: 'Gas',
+        price: +localStorage.getItem('group-1') || 0
+      },
+      {
+        title: 'Luz',
+        price: +localStorage.getItem('group-2') || 0
+      },
+      {
+        title: 'Agua',
+        price: +localStorage.getItem('group-3') || 0
+      },
+      {
+        title: 'Transporte',
+        price: +localStorage.getItem('group-4') || 0
+      }
+    ]
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.finance-wrapper {
+  max-width: 500px;
+}
+.finance-top {
+  width: 100%;
+  height: 67px;
+  border: 3px solid rgba(178, 151, 200, 0.66);
+  margin-bottom: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &__title {
+    color: #000;
+    text-align: center;
+    font-size: 32px;
+    font-style: italic;
+    font-weight: 300;
+    line-height: normal;
+  }
+}
+
+.finance-group {
+  display: flex;
+  margin-bottom: 38px;
+}
+
+.finance-group-left {
+  width: 140px;
+  height: 66px;
+  border-radius: 10px;
+  background-color: #530f7c;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  &__title {
+    color: #fff;
+    text-align: center;
+    font-size: 16px;
+    font-style: italic;
+    font-weight: 800;
+    line-height: normal;
+  }
+
+  &__price {
+    color: #fff;
+    text-align: center;
+    font-size: 14px;
+    font-weight: 500;
+  }
+}
+
+.finance-group-right {
+  width: 210px;
+  height: 66px;
+  margin-right: 15px;
+  border: 1px solid #b297c8;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &__title {
+    height: 35px;
+    width: 90%;
+    color: #000;
+    text-align: center;
+    font-size: 16px;
+    font-style: italic;
+    font-weight: 300;
+    outline: none;
+    border: none;
+  }
+}
+
+.finance-bottom {
+  position: relative;
+  margin: 0 auto;
+  border-radius: 10px;
+  border: 1px solid #b297c8;
+  width: 299px;
+  height: 70px;
+  display: flex;
+  align-items: center;
+  &__left {
+    color: #000;
+    font-size: 12px;
+    font-weight: 400;
+    margin-left: 77px;
+  }
+  &__right {
+    margin-left: 20px;
+  }
+  &__svg {
+    position: absolute;
+    top: 7px;
+    left: 7px;
+  }
+}
+</style>
