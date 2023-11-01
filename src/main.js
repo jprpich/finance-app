198a799fb247ff2d9363b2769a8401f9e3406c05
Joshua Prpich <joshuaprpich@gmail.com>
2023-11-01 13:22:55 -0500

    add vite svg loader

--- src/main.js
+++ src/main.js
@@ -5,10 +5,13 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import InlineSvg from './components/InlineSvg.vue'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.component('InlineSvg', InlineSvg)
 
 app.mount('#app')
