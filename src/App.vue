4c9a373ab99cf33733123a70f04b7ac0b9793904
Joshua Prpich <joshuaprpich@gmail.com>
2023-11-16 12:56:36 -0500

    update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="finance-wrapper">
-    <RouterLink to="/" style="margin-right: 30px">Register</RouterLink>
-    <RouterLink to="/expenses">Expenses</RouterLink>
     <RouterView />
   </div>
 </template>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -61,6 +61,7 @@ export default {
           )
           .then((res) => {
             console.log(res.data)
+            this.$router.push('/expenses')
           })
           .catch((e) => {
             this.errorMessage = e.response.data.message
@@ -73,6 +74,7 @@ export default {
           )
           .then((res) => {
             console.log(res.data)
+            this.$router.push('/expenses')
           })
           .catch((e) => {
             this.errorMessage = e.response.data.message
